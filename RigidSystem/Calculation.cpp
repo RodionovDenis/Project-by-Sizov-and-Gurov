53928a6c2fe369ef03ab5f54dfe894619a25e424
RodionovDenis <denisrodionov5@gmail.com>
2020-01-06 03:42:12 +0300

    rigid

---
+++ RigidSystem/Calculation.cpp
@@ -0,0 +1,40 @@
+﻿#include "Runge.h"
+#include <iostream>
+#include <clocale>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	cout << "Жесткая система." << endl;
+	cout << "Неявный метод Рунге-Кутта оптимального порядка" << endl << endl;
+	double h = 0;
+	int n = 0;
+
+	cout << "Шаг: ";
+	cin >> h;
+	cout << "Количество итераций: ";
+	cin >> n;
+
+	double* vn = new double[2];
+
+	//таблица
+
+	cout << endl << setw(7)<< "N"<<"|" << setw(15) << "x"<<"|" << setw(20) << "u1(x)"<<"|" << setw(20) << "u2(x)"
+		<<"|"<< setw(20) << "v1(x)"<<"|" << setw(20) << "v2(x)"<<"|" << endl;
+	cout << "------------------------------------------------------------------------------------------------------------" << endl;
+
+	for (int i = 0; i <= n; i++)
+	{
+		double x = i * h;
+		PK2(i - 1, h, vn); //вычисление vn значений
+
+		//вывод данных
+
+		cout << setw(7) << i << "|" << setw(15) << x << "|" << setw(20) << u1(x) << "|"
+			<< setw(20) << u2(x) << "|" << setw(20) << vn[0] << "|" << setw(20) << vn[1] << "|" << endl;
+	}
+
+}
---
+++ RigidSystem/Runge.h
@@ -0,0 +1,28 @@
+﻿#include <math.h>
+
+//точное решение
+
+double u1(double x)
+{
+	return 10 * exp(-0.01 * x) - 3 * exp(-1000 * x);
+}
+double u2(double x)
+{
+	return 10 * exp(-0.01 * x) + 3 * exp(-1000 * x);
+}
+
+ //правая часть ДУ
+
+void PK2(int n, double h, double* vn)
+{
+	//неявный метод. значение на n+1 шаге связали с v0 (разложение по базису из собственных векторов)
+
+	double k1 = (1 - 0.005 * h) / (1 + 0.005 * h);
+	double k2 = (1 - 500 * h) / (1 + 500 * h);
+
+	//n+1 шаг
+
+	vn[0] = 10 * pow(k1, n + 1) - 3 * pow(k2, n + 1);
+	vn[1] = 10 * pow(k1, n + 1) + 3 * pow(k2, n + 1);
+
+}
